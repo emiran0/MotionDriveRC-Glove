db35bc68d739c33d2dcb40a143f20bcd0b6cad59
emiran0 <emirhankrtll@gmail.com>
2024-10-30 16:26:53 +0300

    created accelerometer normalization demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,195 @@
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+float normalizeX(float acceleration, float ms2_limit) {
+  float normalized = acceleration / ms2_limit;
+  if (normalized > 1) normalized = 1;
+  else if (normalized < -1) normalized = -1;
+  return normalized;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float normalizeY(float acceleration, float ms2_limit) {
+  float normalized = acceleration / ms2_limit;
+  if (normalized > 2) normalized = 2;
+  else if (normalized < 0) normalized = 0;
+  return normalized;
+}
+
+void displayDataRate(void)
+{
+  Serial.print  ("Data Rate:    "); 
+  
+  switch(accel.getDataRate())
+  {
+    case ADXL345_DATARATE_3200_HZ:
+      Serial.print  ("3200 "); 
+      break;
+    case ADXL345_DATARATE_1600_HZ:
+      Serial.print  ("1600 "); 
+      break;
+    case ADXL345_DATARATE_800_HZ:
+      Serial.print  ("800 "); 
+      break;
+    case ADXL345_DATARATE_400_HZ:
+      Serial.print  ("400 "); 
+      break;
+    case ADXL345_DATARATE_200_HZ:
+      Serial.print  ("200 "); 
+      break;
+    case ADXL345_DATARATE_100_HZ:
+      Serial.print  ("100 "); 
+      break;
+    case ADXL345_DATARATE_50_HZ:
+      Serial.print  ("50 "); 
+      break;
+    case ADXL345_DATARATE_25_HZ:
+      Serial.print  ("25 "); 
+      break;
+    case ADXL345_DATARATE_12_5_HZ:
+      Serial.print  ("12.5 "); 
+      break;
+    case ADXL345_DATARATE_6_25HZ:
+      Serial.print  ("6.25 "); 
+      break;
+    case ADXL345_DATARATE_3_13_HZ:
+      Serial.print  ("3.13 "); 
+      break;
+    case ADXL345_DATARATE_1_56_HZ:
+      Serial.print  ("1.56 "); 
+      break;
+    case ADXL345_DATARATE_0_78_HZ:
+      Serial.print  ("0.78 "); 
+      break;
+    case ADXL345_DATARATE_0_39_HZ:
+      Serial.print  ("0.39 "); 
+      break;
+    case ADXL345_DATARATE_0_20_HZ:
+      Serial.print  ("0.20 "); 
+      break;
+    case ADXL345_DATARATE_0_10_HZ:
+      Serial.print  ("0.10 "); 
+      break;
+    default:
+      Serial.print  ("???? "); 
+      break;
+  }  
+  Serial.println(" Hz");  
+}
+
+void displayRange(void)
+{
+  Serial.print  ("Range:         +/- "); 
+  
+  switch(accel.getRange())
+  {
+    case ADXL345_RANGE_16_G:
+      Serial.print  ("16 "); 
+      break;
+    case ADXL345_RANGE_8_G:
+      Serial.print  ("8 "); 
+      break;
+    case ADXL345_RANGE_4_G:
+      Serial.print  ("4 "); 
+      break;
+    case ADXL345_RANGE_2_G:
+      Serial.print  ("2 "); 
+      break;
+    default:
+      Serial.print  ("?? "); 
+      break;
+  }  
+  Serial.println(" g");  
+}
+
+void setup(void) 
+{
+// #ifndef ESP8266
+//   while (!Serial); // for Leonardo/Micro/Zero
+// #endif
+  Serial.begin(9600);
+  Serial.println("Accelerometer Test"); 
+  Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  // accel.setRange(ADXL345_RANGE_16_G);
+  accel.setRange(ADXL345_RANGE_8_G);
+  // accel.setRange(ADXL345_RANGE_4_G);
+  // accel.setRange(ADXL345_RANGE_2_G);
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Display additional settings (outside the scope of sensor_t) */
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+}
+
+void loop(void) 
+{
+  //Get a new sensor event 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  
+  // Retrieve sensor details for max and min values
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+
+  float X_ms2_limit = 10.0;
+  float Y_ms2_limit = 10.0;
+
+  // Normalize x-axis for direction between -1 and 1
+  float normalizedX = normalizeX(event.acceleration.x, X_ms2_limit);
+  
+  // Normalize y-axis for magnitude between 0 and 2
+  float normalizedY = normalizeY(event.acceleration.y, Y_ms2_limit);
+
+  // Print the normalized values
+  Serial.print("Direction (X): "); Serial.print(normalizedX);
+  Serial.print("  Magnitude (Y): "); Serial.println(normalizedY);
+  
+  delay(300);
+
+  // /* Get a new sensor event */ 
+  // sensors_event_t event; 
+  // accel.getEvent(&event);
+ 
+  // /* Display the results (acceleration is measured in m/s^2) */
+  // Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  // Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  // Serial.println("m/s^2 ");
+  // delay(200);
+  
+  
 }
